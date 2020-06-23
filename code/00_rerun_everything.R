4955e9fbfef113806e4061c6d2d5e6dbc971420f
Mathew Kiang <mathew.kiang@gmail.com>
2020-06-23 11:44:28 -0700

    Fix the rendering

--- code/00_rerun_everything.R
+++ code/00_rerun_everything.R
@@ -1,5 +1,5 @@
 ## 00_rerun_everything.R ----
-## Just re-runs everything for data updates. Convenience script. 
+## Just re-runs everything for data updates. Convenience script.
 
 ## Reanalyze data ----
 source("./code/01_process_raw_data.R")
@@ -10,15 +10,17 @@ source("./code/03_plot_keywords_by_institution.R")
 source("./code/99_plot_fig1_private.R")
 
 ## Reknit results and descriptives ----
-knitr::knit("./rmds/01_descriptives.Rmd",
-            "./rmds/01_descriptives.html")
+rmarkdown::render(input = "./rmds/01_descriptives.Rmd",
+                  output_file = "01_descriptives.html",
+                  output_dir = "./rmds/")
 
-knitr::knit("./rmds/02_evaluating_statements.Rmd",
-            "./rmds/02_evaluating_statements.html")
+rmarkdown::render(input = "./rmds/02_evaluating_statements.Rmd",
+                  output_file = "02_evaluating_statements.html",
+                  output_dir = "./rmds/")
 
 ## Reknit the github README ----
-knitr::knit("./README.Rmd",
-            "./README.md")
+rmarkdown::render(input = "./README.Rmd",
+                  output_file = "README.md")
 
 ## Save session information ----
 sink(here("session_info.txt"))
